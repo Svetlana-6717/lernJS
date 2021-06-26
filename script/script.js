a51c9daa544ce5c9b22749ce035aea0eddd3e3be
Svetlana <sl1248@yandex.ru>
2021-06-27 01:10:44 +0300

    lesson24 complete

--- script/script.js
+++ script/script.js
@@ -280,7 +280,7 @@ window.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    //валидация форм
+    //валидация форм регулярками
 
     const calcSquare = document.querySelector('.calc-square');
     const calcCount = document.querySelector('.calc-count');
@@ -374,6 +374,53 @@ window.addEventListener('DOMContentLoaded', function () {
             inputMessage.value = inputMessage.value.replace(/-{2,}/g, '-');
         }
     });
+
+    //калькулятор
+
+    const calc = () => {
+
+        const calcBlock = document.querySelector('.calc-block');
+        const calcType = document.querySelector('.calc-type');
+        const calcSquare = document.querySelector('.calc-square');
+        const calcDay = document.querySelector('.calc-day');
+        const calcCount = document.querySelector('.calc-count');
+        const totalValue = document.getElementById('total');
+
+        const countSum = (price = 100) => {
+            let total = 0;
+            let countValue = 1;
+            let dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const squareValue = +calcSquare.value;
+
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+
+    };
+
+    calc(100);
 
 });
 
